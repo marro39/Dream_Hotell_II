883e26a37cf7fc2760a0096837f737fdd9b0c629
Marcus <marcusronnang@hotmail.com>
2013-11-11 16:02:32 +0100

    Updating login mainly, small css config for whole site,

--- application/views/login.php
+++ application/views/login.php
@@ -14,11 +14,12 @@
 					<?php 
 						$passwordData=array(
 							'name' => 'password',
-							'id' => 'password'						
+							'id' => 'password',
+							'class' => 'password'						
 						);
 						echo form_password($passwordData);						
 					?>
-						<input type="checkbox" id=""/>
+						<input type="checkbox" class="showPassword"/>
 					<?php
 						$submitData=array(
 							'name' => 'buttonSubmit',
@@ -32,9 +33,10 @@
 					<p id="pCreateAccount">Click <a href="#" id="createAccount">here</a> to create a new account</p>
 			</div>
 			<div id="createAccountContent">
-				<label for="firstName" class="labelInput">Firstname </label><span class="redColor"> *</span>
+				<?php echo form_open('main/createAccount');?>
+				<label for="firstName" class="labelInput">Firstname:</label><span class="redColor">&nbsp&nbsp&nbsp</span>
 				<?php					
-					echo form_open('main/createAccount');
+					
 					$firstName=array(
 						'id' => 'firstName',
 						'name' => 'firstName',
@@ -42,7 +44,7 @@
 					);
 					echo form_input($firstName);
 				?>
-				<br /><label for="lastName" class="labelInput">Lastname </label><span class="redColor">*</span>
+				<br /><label for="lastName" class="labelInput">Lastname:</label><span class="redColor">&nbsp&nbsp&nbsp</span>
 				<?php					
 					$lastName=array(
 						'id' => 'lastName',
@@ -51,9 +53,34 @@
 					);
 					echo form_input($lastName);
 				?>
-				<label for="email1" class="labelInput">Username </label><span class="redColor">*</span>
-				<input type="email" name="email1" id="email1" value="someone@hotmail.com"/><br />
-				
+				<br /><label for="email1" class="labelInput">Username:</label><span class="redColor"> *</span>
+				<input type="email" name="email1" id="email1" value="someone@hotmail.com"/>
+				<br /><label for="newPassword" class="labelInput">Password:</label><span class="redColor"> *</span>
+				<?php 
+					$passwordData1=array(
+						'id' => 'password1',
+						'name' => 'password1',
+						'class'=> 'password'
+					);
+					echo form_password($passwordData1);
+				?>
+				<input type="checkbox" class="showPassword"/>
+				<br /><label for="newPasswordCheck" class="labelInput">Repeat password:</label><span class="redColor"> *</span>
+				<?php 
+					$passwordData2=array(
+						'id' => 'password2',
+						'name' => 'password2',
+						'class'=> 'password'
+					);
+					echo form_password($passwordData2);
+					$submitData1=array(
+							'name' => 'buttonSubmit1',
+							'id' => 'buttonSubmit1',
+							'class' => 'button0',
+							'value' => 'Apply'						
+						);
+						echo form_submit($submitData1);
+				?>
 				
 				<?php
 					echo form_close();
@@ -63,4 +90,5 @@
 		</div>			
 </section>
 <aside></aside>
+<script type="text/javascript" src="<?php echo base_url()?>public/js/login.js"></script>
 
---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('.showPassword').change(function(){
+		
+		var type = $('.password').attr('type');
+		
+		
+		if(type =='password'){
+			$('.password').attr('type','text');
+			alert('Password');
+		}
+		else{
+			$('.password').attr('type','password');
+			alert('Text');
+		}
+		
+	});
+	$('#createAccount').click(function(){
+		$('#inputContainer').slideUp(1000, function(){
+			$('#createAccountContent').slideDown(function(){
+				$('#headerTitle').text('Create account');				
+			});
+		});
+	});
+});
